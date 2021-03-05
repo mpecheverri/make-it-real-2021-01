b7f03f012fe758250868f1380264678d03b39c37
Carlos Guzman <cags84@gmail.com>
2021-03-04 21:40:50 -0500

    Agregamos la solución del ejercicio de objetos

--- 03-node/05/ejercicio/solucion-ejercicio-objetos.js
+++ 03-node/05/ejercicio/solucion-ejercicio-objetos.js
@@ -1,1 +1,73 @@
-console.log("Carlos Guzmán");
+console.log("");
+console.log("Autor: Carlos Guzmán");
+console.log("");
+/* 
+    Ejercicio:
+
+    Obtener la edad de las personas que hablen inglés de un arreglo de 5 objetos Persona. Cada elemento del arreglo tiene la siguiente estructura:
+    [{
+        nombre: "Jesus",
+        sexo: "M",
+        edad: 36,
+        idiomas: ["EN","ES"],
+        verEdad: function(){
+            console.log(`La edad de ${this.nombre} es ${this.edad}`);
+        }
+    }, ...]
+
+*/
+
+/* Solución plateada */
+
+const personas = [
+    {
+      nombre: "Carlos",
+      sexo: "M",
+      edad: 36,
+      idiomas: ["EN","ES"],
+      verEdad: function(){
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+      }
+    },
+    {
+      nombre: "Pedro",
+      sexo: "M",
+      edad: 26,
+      idiomas: ["EN","ES"],
+      verEdad: function(){
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+      }
+    },
+    {
+      nombre: "Jesus",
+      sexo: "M",
+      edad: 20,
+      idiomas: ["ES"],
+      verEdad: function(){
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+      }
+    },
+    {
+      nombre: "Sebastian",
+      sexo: "M",
+      edad: 25,
+      idiomas: ["EN"],
+      verEdad: function(){
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+      }
+    },
+    {
+      nombre: "Adolfo",
+      sexo: "M",
+      edad: 66,
+      idiomas: ["ES"],
+      verEdad: function(){
+        console.log(`La edad de ${this.nombre} es ${this.edad}`);
+      }
+    }
+  ];
+
+
+  // Solución al ejercicio.
+  let personasIngles = personas.filter(persona => persona.idiomas.includes("EN"));
+  personasIngles.map(el => el.verEdad());
