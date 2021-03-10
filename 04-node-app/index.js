a632d6110e95f9572ea7024f2af2ed7a657ee3c3
Jesus Estrada <jesuestrada@gmail.com>
2021-03-09 19:59:59 -0500

    bcrypt

--- 04-node-app/index.js
+++ 04-node-app/index.js
@@ -1,1 +1,13 @@
 //encriptar una cadena utilizando bcrypt
+
+const argumentos = process.argv;
+const cadenaEntrada = argumentos.slice(2).join(' ');
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const salt = bcrypt.genSaltSync(saltRounds);
+const hash = bcrypt.hashSync(cadenaEntrada, salt);
+
+console.log(cadenaEntrada);
+console.log(hash);
