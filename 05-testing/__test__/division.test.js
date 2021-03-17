1f36a8bff5db285dc22f3bfcb16641f25f35341f
Jesus Estrada <jesuestrada@gmail.com>
2021-03-16 20:57:15 -0500

    beforeEach

--- 05-testing/__test__/division.test.js
+++ 05-testing/__test__/division.test.js
@@ -1,8 +1,18 @@
 const { division } = require('./../modules/division');
 
+beforeEach(() => {
+  console.log(`beforeEach ${new Date().toLocaleString()}`);
+});
+
+afterEach(() => {
+  console.log(`afterEach ${new Date().toLocaleString()}`);
+});
 describe('division', () => {
-  test('2 numbers', () => {
+  test('2 numbers to be', () => {
     expect(division(5, 4)).toBe(1.25);
+  });
+  test('2 numbers not be', () => {
+    expect(division(1, 1)).not.toBe(0);
   });
   test('by zero', () => {
     expect(division(5, 0)).toBe('no se puede realizar');
