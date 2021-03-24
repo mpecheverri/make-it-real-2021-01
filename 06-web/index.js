e99996b9bae373b562bae44105133a0a49360357
Jesus Estrada <jesuestrada@gmail.com>
2021-03-23 21:01:17 -0500

    middlewares

--- 06-web/index.js
+++ 06-web/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { users } = require('./users');
+const { log } = require('./middleware/log');
 const app = express();
 const port = 3000;
 
-//get json objects from body
 app.use(express.json());
+app.use(log);
 
 app.get('/', (req, res) => {
   res.send('Hello world');
@@ -14,23 +15,33 @@ app.get('/users', (req, res) => {
   res.json(users);
 });
 
+app.post('/users', (req, res) => {
+  const { name, email, username, password } = req.body;
+
+  if (name && email && username && password) {
+    const user = {
+      name,
+      email,
+      username,
+      password,
+    };
+
+    const found = users.filter((u) => u.username === user.username); //[], [{}]
+
+    if (found && found.length > 0) {
+      res.json({ message: `ya existe el usuario ${user.username}` });
+    } else {
+      users.push(user);
+      res.json(users);
+    }
+  } else {
+    res.json({ message: `hay datos nulos` });
+  }
+});
+
 app.get('/users/:username', (req, res) => {
   const username = req.params.username;
   res.send(`User page ${username}`);
-});
-
-app.post('/users', (req, res) => {
-  //optimistic save
-  const user = {
-    name: req.body.name,
-    email: req.body.email,
-    username: req.body.username,
-    password: req.body.password,
-  };
-
-  users.push(user);
-
-  res.json(users);
 });
 
 app.listen(port, () => {
---
+++ 06-web/middleware/log.js
@@ -0,0 +1,9 @@
+const log = (req, res, next) => {
+  const datetime = new Date().toLocaleString();
+  const { ip = '', method = '', path = '', hostname = '' } = req;
+  console.log(`${datetime} :: ${method} :: ${hostname} :: ${path} :: ${ip}`);
+
+  next();
+};
+
+module.exports = { log };
