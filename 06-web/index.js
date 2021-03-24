9ffcffc98237f486301cb8af432246bbb182594d
Jesus Estrada <jesuestrada@gmail.com>
2021-03-23 20:00:47 -0500

    post and nodemon

--- 06-web/index.js
+++ 06-web/index.js
@@ -3,11 +3,33 @@ const { users } = require('./users');
 const app = express();
 const port = 3000;
 
+//get json objects from body
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
 app.get('/users', (req, res) => {
+  res.json(users);
+});
+
+app.get('/users/:username', (req, res) => {
+  const username = req.params.username;
+  res.send(`User page ${username}`);
+});
+
+app.post('/users', (req, res) => {
+  //optimistic save
+  const user = {
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  users.push(user);
+
   res.json(users);
 });
 
