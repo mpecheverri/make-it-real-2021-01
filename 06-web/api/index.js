d1ec47d1430c838bef18d7234babff6d7214709f
Jesus Estrada <jesuestrada@gmail.com>
2021-04-08 20:59:10 -0500

    helmet

--- 06-web/api/index.js
+++ 06-web/api/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
 const fs = require('fs');
 const path = require('path');
 const { localization } = require('./middleware/localization');
@@ -16,6 +17,7 @@ const logStream = fs.createWriteStream(path.join(__dirname, accessLogDir), {
   flags: 'a',
 });
 
+router.use(helmet());
 router.use(morgan('combined', { stream: logStream }));
 router.use(localization);
 router.use('/users', users);
